9cab76b089a828354f0a95ed0d0428fc82df8a11
vval56 <pitbulkodeinoslav@gmail.com>
2025-10-02 19:06:55 +0300

    null

--- lab_3/src/main.cc
+++ lab_3/src/main.cc
@@ -1,8 +1,8 @@
 #include "../headers/menu.h"
 
 int main(){
-    Shape * shape1;
-    Shape * shape2;
+    Shape * shape1 = nullptr;
+    Shape * shape2 = nullptr;
     menu(shape1, shape2);
     return 0;
 }
