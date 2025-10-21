53040afffe353f3fb7c835a7fa8269ecb4db1248
vval56 <pitbulkodeinoslav@gmail.com>
2025-10-21 14:28:40 +0300

    7.5

--- lab_7/header/user.h
+++ lab_7/header/user.h
@@ -16,18 +16,16 @@ public:
     
     static User** load_users_from_file(const std::string& file_name, int& user_count);
     static void free_users(User** users, int count);
-    void display() const;
     
-    std::string get_surname() const { return surname_; }
-    std::string get_name() const { return name_; }
-    std::string get_middle_name() const { return middle_name_; }
-    int get_number() const { return number_worker_; }
+    void display() const;
 
 private:
     std::string surname_;
     std::string name_;
     std::string middle_name_;
     int number_worker_;
+    static void reset_user_data(std::string& surname, std::string& name, std::string& middle_name, int& number);
+    static void process_data_line(const std::string& line, size_t pos, std::string& surname, std::string& name, std::string& middle_name, int& number);
 };
 
 #endif
